690cc7ebb4a0ff651b6b18d3a3cd02cda5cd52fd
JGSuw <jgs6156@uw.edu>
2013-07-29 16:36:40 -0700

    RPI GPIO testing script

---
+++ rpi_gpio/gpiotesting.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+import threading
+import datetime
+
+GPIO.setmode(GPIO.BCM)
+
+class door_listener(threading.Thread):
+    def run(self):
+        now = datetime.datetime.now()
+        while 1:
+            GPIO.wait_for_edge(self.in_pin,GPIO.BOTH)
+            if GPIO.output(self.out_pin) == GPIO.HIGH:
+                print 'close'
+                GPIO.output(self.out_pin,GPIO.LOW)
+            else:
+                print 'open'
+                GPIO.output(self.out_pin,GPIO.LOW)
+        pass
+    def __init__(self,in_pin,out_pin):
+        self.in_pin = in_pin
+        self.out_pin = out_pin
+        threading.Thread.__init__(self)
+        pass
+    def stop(self):
+        GPIO.cleanup()
+        threading.Thread.__stop()
+        pass
+    
+    
+    
+if __name__ == '__main__':
+    in_pin = raw_input("Enter input pin header (BOARD): ")
+    out_pin = raw_input("Enter output pin header (BOARD): ")
+    
+    GPIO.setup(in_pin,GPIO.IN)
+    GPIO.setup(out_pin,GPIO.OUT)
+    
+    if GPIO.input(in_pin) == GPIO.LOW:
+        print 'Initially open'
+        GPIO.output(out_pin,GPIO.HIGH)
+    else:
+        print 'Initially closed'
+        GPIO.output(out_pin,GPIO.LOW)
+    
+    listener = door_listener(in_pin,out_pin)
+    listener.start()
+    listener.stop
+    
+    raw_input("Enter anything to cease execution: ")
+    
+    pass
